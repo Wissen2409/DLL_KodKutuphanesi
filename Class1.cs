3a2e25e4b3c1aac5a98ce3656f809809cf42a6f1
emrahelis <emrahelis@gmail.com>
2024-10-27 12:28:38 +0300

    first commit

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace DLL_KodKutuphanesi;
+
+public class Ogrenci
+{
+
+    //dotnet new classlib komutu ile bir class Library oluşturduk!!
+    // Exe ile DLL arasındaki en önemli fark : DLL'lerde main metodu yoktur!!
+    // DDL dosyaları tek başlarına çalıştırılamazlar!!!
+    // Her exe uygulamada kesinlikle bir main metot olmalıdır!!!
+    // DLL dosyalarında main metot olmadığı için çalıştırılamazlar, zaten çalıştırmak için değil
+    // başka uygulamalara yardımcı olmak için varlardır!!
+
+    // dll dosyasını oluşturmak için , terminal ekranına dotnet build yazmanız yeterlidir!!
+    public void Calis(){
+        Console.WriteLine("Ögrenci çalıştı");
+    }
+    public void Uyu(){
+        Console.WriteLine("Öğrenci uyudu!!!");
+    }
+    // 
+    
+
+}
